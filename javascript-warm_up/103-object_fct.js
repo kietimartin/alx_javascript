d07dedb32c643f97f1ecf203140e0f339e45dbd1
martinkieti <martinkieti6@gmail.com>
2023-09-12 22:18:13 +0300

    Increment

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
+
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  /*
-  YOUR CODE HERE
-  */
-function incr (value) {
-    myObject.value = value;
-    value += 1;
-    return value
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+/*
+YOUR CODE HERE
+*/
+myObject.incr = function () {
+    myObject.value++
 };
 
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
