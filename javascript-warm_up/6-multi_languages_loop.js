cd740199e413e38bf93b2d29f2865711c3459aee
martinkieti <martinkieti6@gmail.com>
2023-09-12 21:34:24 +0300

    Loops

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 const multiLanguages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 // looping using for loop
-for (let i = 0; i == multiLanguages.length; i++) {
+for (let i = 0; i < multiLanguages.length; i++) {
     console.log(multiLanguages[i])
 };
