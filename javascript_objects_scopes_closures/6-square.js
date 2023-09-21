75d21e70ca0b333026743aad542d91f334e3f9ba
martinkieti <martinkieti6@gmail.com>
2023-09-21 22:48:24 +0300

    fixing errors

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,47 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor(w, h) {
+        if (w > 0 && h > 0){
+            this.width = w;
+            this.height = h;
+        }
+    }
+
+    print() {
+        for (let i = 0; i < this.height; i++) {
+            console.log('X'.repeat(this.width));
+        }
+    }
+
+    rotate () {
+        [this.width, this.height] = [this.height, this.width];
+    }
+
+    double () {
+        this.height *= 2;
+        this.width *= 2;
+    }
+}
+
+class Square extends Rectangle {
+    constructor (size) {
+        super(size, size);
+    }
+}
+module.exports = Rectangle;
+module.exports = Square;
+
+class Square extends Square {
+    charPrint(c) {
+        if (c === undefined) {
+            c = 'X';
+        }
+
+        for (let i = 0; i < this.height; i++) {
+            console.log(c.repeat(this.width));
+        }
+    }
+}
+
+module.exports = Square;
