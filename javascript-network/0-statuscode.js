49d4a1bc63d724c32b682a367a5ff0dd73deec76
martinkieti <martinkieti>
2023-09-27 12:32:44 +0300

    status code

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -4,6 +4,7 @@ const req = require('request');
 
 const url = process.argv[2];
 
-req.get(url, function(response) {
+req.get(url, {encoding: 'utf-8'} )
+.on('response', response => {
     console.log('code: ', response.statusCode);
 });
