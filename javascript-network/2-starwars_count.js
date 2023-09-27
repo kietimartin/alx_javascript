1008a8e0ee5196c2d6e1d8a6380bc0f756dd39e3
martinkieti <martinkieti>
2023-09-27 20:39:08 +0300

    character counter

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -7,17 +7,17 @@ const char_id = process.argv[2];
 const searchQuery = `https://swapi.dev/api/people/${id}/`;
 const url = 'https://swapi-api.alx-tools.com/api/films/';
 
-req.get(url, function (body) {
+req.get(url, function (body, error, response) {
     // parsing the JSON into an object
-    const result = JSON.parse(body);
-
-    for (i = 0, i in result.characters[i], i++) {
-        if (result.characters[i] === searchQuery){
-            count += 1;
-        }
-        else {
-            count = count;
-        }
-        console.log(count);
+    if (error) {
+        console.log(error);
+    } else if (response.statusCode === 200) {
+        const js_object = JSON.parse(body).results;
+        const characterId = '18';
+        const wedgeAntilles = js_object.filter(counter=>{
+            return counter.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
+        })
+    } else {
+        console.log('code:' +response.statusCode);
     }
-})
+});
