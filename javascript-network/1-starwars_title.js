ee6e99cfde322a27e9d7cec41d74f8e44776d873
martinkieti <martinkieti>
2023-09-27 12:42:00 +0300

    starwars movie title

---
+++ javascript-network/1-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const req = require('request');
+// the starwars api enpoind link
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+// the request function
+req.get(url, {encoding: 'utf-8'})
+    .on('data', data => {
+        const result = JSON.parse(data);
+        console.log(result.results.title);
+    });
