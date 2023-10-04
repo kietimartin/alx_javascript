54546609dbaee1e480395e22a7d3997afcc97645
martinkieti <martinkieti6@gmail.com>
2023-10-04 23:28:18 +0300

    cookies

---
+++ html_js_validation/0-password-strength.js
@@ -0,0 +1,9 @@
+/*This script retrieves the value entered in the password input filed.
+It checks if the password is;
+    *At least 8 characters long.
+    *At least one uppercase.
+    *At least one lowercase.
+    *At least one numeric digit
+    *At least one special character
+*/
+
---
+++ html_js_validation/README.md
@@ -0,0 +1,6 @@
+# FORM VALIDATION
+There are 2 client-side validation options
+1. Javascript validation.
+2. Built-in HTML validation.
+
+This directory focusses on using Js to validate data sent to the servers.
